c9209789c91be1cfed07dc1d362f95f0d3ecaaaf
kemmal <kemmalAlx@gamil.com>
2024-03-22 20:13:04 +0000

    python

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def islower(c):
-    return ord(c) >= ord('A') and ord(c) <= ord('Z')
+def uppercase(str):
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            new = chr(ord(c) - 32)
+        else:
+            new = c
+        print(new, end="")
+    print("")
