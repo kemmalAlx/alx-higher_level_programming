cf9060158c59f87f63bdbeb7a9bb2dc774672582
kemmal <kemmalAlx@gamil.com>
2024-03-22 18:05:22 +0000

    python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 for nbr in range(100):
-    if nbr < 10:
-        print('0{}'.format(nbr), end="")
+    if (nbr == 99):
+        print("{}".format(nbr))
     else:
-        print("{}".format(nbr), end="")
-    if nbr != 99:
-        print(", ", end="")
-    else:
-        print(end="\n")
+        print("{:02}".format(nbr), end=", ")
