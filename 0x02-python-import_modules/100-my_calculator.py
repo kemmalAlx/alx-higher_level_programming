4dd0ddbde02c8a42d023cbde9cb28974c2c04ad2
kemmal <kemmalAlx@gamil.com>
2024-03-23 19:35:37 +0000

    python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,24 +3,24 @@
 if __name__ == "__main__":
     """100-my_calculator.py"""
 
-    from calculator_1 import add, sub, mul, div
-    from sys import argv
+from calculator_1 import add, sub, mul, div
+from sys import argv
 
 
-    if len(argv)-1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
-    if op == '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op == '*':
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op == '-':
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op == '/':
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+if len(argv) - 1 != 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+a = int(argv[1])
+op = argv[2]
+b = int(argv[3])
+list = {
+    '+': add,
+    '-': sub,
+    '*': mul,
+    '/': div
+}
+if op in list.keys():
+    print("{} {} {} = {}".format(a, op, b, list[op]))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
