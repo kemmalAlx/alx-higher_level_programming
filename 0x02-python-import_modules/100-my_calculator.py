06b1afb88625a57aaa8e4484bc105237ac7f28f3
kemmal <kemmalAlx@gamil.com>
2024-03-23 17:06:25 +0000

    python

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """100-my_calculator.py"""
+
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+
+    if len(argv) - 1 != 3:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    if op == '+':
+        print("{} {} {} = {}".format(a, op, b, add(a, b)))
+    if op == '*':
+        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+    if op == '-':
+        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+    if op == '/':
+        print("{} {} {} = {}".format(a, op, b, div(a, b)))
