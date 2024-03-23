8af529c8c8489077d0aaf7959ead587fd68fce83
kemmal <kemmalAlx@gamil.com>
2024-03-23 15:05:53 +0000

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,9 @@ len = len(sys.argv) - 1
 if len == 0:
     print("0 arguments.")
 else:
-    print("{} argument:".format(len))
+    if len == 1:
+        print("{} argument:".format(len))
+    else:
+        print("{} arguments:".format(len))
     for i in range(1, len + 1):
         print("{}: {}".format(i, sys.argv[i]))
