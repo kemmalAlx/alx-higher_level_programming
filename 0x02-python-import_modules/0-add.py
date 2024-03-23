d36ce80f2d4e7d8d286e380463b3d1b18d898c49
kemmal <kemmalAlx@gamil.com>
2024-03-23 14:37:36 +0000

    python

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,9 +2,9 @@
 
 if __name__ == "__main__":
     """To print the sum of 1 and 2."""
-    
-def add(a, b):
+
+    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, a + b))
-    return a + b
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
