f180879454ff3c16f520dfc00ed67c89781aa9c9
kemmal <kemmalAlx@gamil.com>
2024-03-23 13:40:44 +0000

    python

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-pow = __import__('11-pow').pow
+remove_char_at = __import__('101-remove_char_at').remove_char_at
 
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+print(remove_char_at("Best School", 3))
+print(remove_char_at("Chicago", 2))
+print(remove_char_at("C is fun!", 0))
+print(remove_char_at("School", 10))
+print(remove_char_at("Python", -2))
