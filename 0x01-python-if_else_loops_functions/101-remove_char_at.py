25fa414353de61dbcb681a3e8fdbe00bd40d5b44
kemmal <kemmalAlx@gamil.com>
2024-03-23 13:47:48 +0000

    python

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,6 +4,6 @@ def remove_char_at(str, n):
     newStr = ""
     for i in range(0, len(str)):
         if (i != n):
-            newStr +=str[i]
+            newStr += str[i]
         i += 1
     return newStr
