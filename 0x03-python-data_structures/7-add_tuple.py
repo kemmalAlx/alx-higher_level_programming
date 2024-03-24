4787fd3b112cb9a6596a94d828bf8cecbb742f77
kemmal <kemmalAlx@gamil.com>
2024-03-24 16:52:16 +0000

    python

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,21 +11,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len_b == 1:
         tuple_b = tuple_b[0], 0
     return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
-
-""" 
-To do
-if t_a < 2
-    use 0 for each messing
-if t_b < 2
-    use 0 for each messing
-    
-"""
