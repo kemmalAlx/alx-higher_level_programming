e329e47faf792c250b820f1be1cbf994b38deaa2
kemmal <kemmalAlx@gamil.com>
2024-03-24 08:04:59 +0000

    python

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,9 +2,5 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
-    new_list = []
-    for i, list in enumerate(my_list):
-        if i != idx:
-            new_list.append(list)
-    my_list = new_list
+    del my_list[idx]
     return my_list
