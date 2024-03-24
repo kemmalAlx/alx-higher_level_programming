706d1cafea864190f3a1f82a3657c6962e5dae3e
kemmal <kemmalAlx@gamil.com>
2024-03-24 01:01:19 +0000

    python

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,8 +3,5 @@ def replace_in_list(my_list, idx, element):
     count = len(my_list)
     if idx < 0 or idx >= count:
         return None
-    new_list = []
-    for list in my_list:
-        new_list.append(list)
-    new_list[idx] = element
-    return new_list
+    my_list[idx] = element
+    return my_list
