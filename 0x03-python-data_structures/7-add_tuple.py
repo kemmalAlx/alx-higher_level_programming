ba39d53e8469ea083231b4e88a4d20a45ca1bc81
kemmal <kemmalAlx@gamil.com>
2024-03-24 16:54:16 +0000

    python

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,4 @@
-#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_a = len(tuple_a)
-    len_b = len(tuple_b)
-    if len_a == 0:
-        tuple_a = 0, 0
-    if len_a == 1:
-        tuple_a = tuple_a[0], 0
-    if len_b == 0:
-        tuple_b = 0, 0
-    if len_b == 1:
-        tuple_b = tuple_b[0], 0
-    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return a[0] + b[0], a[1] + b[1]
