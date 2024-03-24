b05848d131bd4ada468d593e3ccc99b39b75d82f
kemmal <kemmalAlx@gamil.com>
2024-03-24 01:54:17 +0000

    python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,10 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         print()
-    for i in matrix:
-        for j in i:
-            if j != i[-1]:
-                print("{:d}".format(j), end=" ")
-            else:
-                print("{:d}".format(j))
+    else:
+        for i in matrix:
+            for j in i:
+                if j != i[-1]:
+                    print("{:d}".format(j), end=" ")
+                else:
+                    print("{:d}".format(j))
