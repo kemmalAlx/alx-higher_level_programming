a8076bfc1a6a98f7b3720a022bd4ea7c95a2ec62
kemmal <kemmalAlx@gamil.com>
2024-03-25 20:14:41 +0000

    python

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 # my code
-# def My_multiply_by_2(a_dictionary):
-#     new_dict = {}
-#     for key in a_dictionary:
-#         new_dict[key] = a_dictionary[key] * 2
-#     return new_dict
+def My_multiply_by_2(a_dictionary):
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
 
 # simplified code from copilot
 def multiply_by_2(a_dictionary):
     return {key: val * 2 for key, val in a_dictionary.items()}
-
-def test():
-    val = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
-    for key, value in val.items():
-        print("key: {}, val: {}".format(key, value))
-
-test()
