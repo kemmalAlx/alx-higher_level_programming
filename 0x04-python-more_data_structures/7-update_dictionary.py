219a4d9f9509e40326ead0bcfa1eca02b8af3008
kemmal <kemmalAlx@gamil.com>
2024-03-25 18:48:06 +0000

    python

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     new = {key: value}
-    newDir = a_dictionary.update(new)
-    return newDir
-
-# print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
-
-# a_dictionary = { 'a': "a", 'b': "b" , 'c': "c", 'd': "d", 'e': "e" } 
-# key = "a" 
-# value = "A"
-
-
-
-# new_dict = update_dictionary(a_dictionary, key, value)
-# print_sorted_dictionary(new_dict)
-# print("--")
-# print_sorted_dictionary(a_dictionary)
+    a_dictionary.update(new)
+    return a_dictionary
