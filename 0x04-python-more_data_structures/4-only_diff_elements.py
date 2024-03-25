af59235e4559a54dbbf4e1ad866188ceef01e18c
kemmal <kemmalAlx@gamil.com>
2024-03-25 15:42:16 +0000

    python

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,23 +1,4 @@
 #!/usr/bin/python3
-
-# my code
-"""
-def My_only_diff_elements(set_1, set_2):
-    new_set = []
-    for s in set_1:
-        new_set.append(s)
-    for s in set_2:
-        new_set.append(s)
-    my_dict = {i : new_set .count(i) for i in new_set}
-    ret = []
-    for m in my_dict:
-        if my_dict[m] == 1:
-            ret.append(m)
-    return ret
-"""
-
 # simplifier code by Copilot
-
 def only_diff_elements(set_1, set_2):
-
     return set_1 ^ set_2
