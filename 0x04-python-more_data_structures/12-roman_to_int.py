913dd1cf4d421747d7855dc5d81f6b227b5fd38a
Abdelhamid EL EKMMAL <abdelhamidelekmmal@Abdelhamids-MacBook-Pro.local>
2024-03-29 18:41:07 +0000

    python

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
+roman = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+    "MM": 2000,
+    "MMM": 3000,
+    "MMMM": 4000,
+}
+
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
+    total = 0
+    tmp = 0
+    for r in reversed(roman_string):
+        if roman[r] >= tmp:
+            total -= roman[r]
+        else:
+            total += roman[r]
+        tmp = roman[r]
+    return abs(total)
