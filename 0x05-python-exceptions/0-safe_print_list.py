fe6ca9f7330cbefd8d2d42b007e4453c7fb1ec30
kamal <kemmalalx@gmail.com>
2024-04-06 00:25:51 +0000

    python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def safe_print_list(my_list = [], x = 0):
+def safe_print_list(my_list=[], x=0):
     try:
         len = 0
         for i, list in enumerate(my_list):
             if i < x:
-                len+=1
+                len += 1
                 print(list, end="")
         print()
         return len
---
+++ 0x05-python-exceptions/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+safe_print_list = __import__('0-safe_print_list').safe_print_list
+
+my_list = [1, 2, 3, 4]
+x = len(my_list) + 10
+
+nb_print = safe_print_list(my_list, x)
+print("{:d}".format(nb_print))
